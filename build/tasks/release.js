e1066663e1e2fac2849816a0e2090bed05a6be28
Micah <micah@zoltu.net>
2015-04-20 06:51:03 -0700

    Adds release prep build task.

---
+++ build/tasks/release.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var jsonEditor = require('gulp-json-editor');
+
+gulp.task('jspm-prepare-release', ['build'], function() {
+	mutatePackagesJson = function(json) {
+		delete json.devDependencies;
+		delete json.dependencies;
+		delete json.main;
+		json.jspm.directories.lib = ".";
+		return json;
+	};
+	jsonOptions = {
+		'indent_char': '\t',
+		'indent_size': 1
+	};
+	gulp.src('./package.json')
+		.pipe(jsonEditor(mutatePackagesJson, jsonOptions))
+		.pipe(gulp.dest('./output/system'));
+});

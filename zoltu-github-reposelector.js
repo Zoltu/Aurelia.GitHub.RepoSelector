29bd1f7352807429ebad11259a40411db3527169
Micah <micah@zoltu.net>
2015-04-20 06:54:48 -0700

    First!

---
+++ zoltu-github-reposelector.js
@@ -0,0 +1,18 @@
+System.register([], function (_export) {
+  var _classCallCheck, ZoltuGithubRepoSelector;
+
+  return {
+    setters: [],
+    execute: function () {
+      "use strict";
+
+      _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+      ZoltuGithubRepoSelector = function ZoltuGithubRepoSelector() {
+        _classCallCheck(this, ZoltuGithubRepoSelector);
+      };
+
+      _export("ZoltuGithubRepoSelector", ZoltuGithubRepoSelector);
+    }
+  };
+});
